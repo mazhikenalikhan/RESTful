d85a1484c565b015d668889c840ab63e013eb4e9
Alikhan <mazhiken.alikhan@gmail.com>
2022-07-27 12:13:43 +0600

    First

---
+++ src/main/java/com/example/payrool/PayroolApplication.java
@@ -0,0 +1,13 @@
+package com.example.payrool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayroolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayroolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/payrool/PayroolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payrool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayroolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
